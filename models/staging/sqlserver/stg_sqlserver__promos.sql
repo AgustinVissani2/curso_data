86a45c4e81a2ac94c4f8706de8f6fd8ce911e854
Agustin Vissani <vissani1997@gmail.com>
2024-05-28 17:53:39 +0000

    last updated

--- models/staging/sqlserver/stg_sqlserver__promos.sql
+++ models/staging/sqlserver/stg_sqlserver__promos.sql
@@ -1,15 +1,26 @@
 with
-    source as (select * from {{ source("_sqlserver_sources", "promos") }}),
+    source as (
+        select * from {{ source("_sqlserver_sources", "promos") }}
+    ),
     src_sqlserver as (
         select
             md5(promo_id) as promo_id,
             promo_id as promo_name,
             discount as discount_euro,
-            status as id_status,
+            iff(status = 'inactive', 0, 1) as status_id,
             status,
-            _fivetran_deleted  as date_deleted,
-            _fivetran_synced as date_load
-            from source
+            _fivetran_deleted as date_deleted,
+            convert_timezone('UTC', _fivetran_synced) as _fivetran_synced_utc
+        from source
+        union all
+        select
+            md5('Stranger') as promo_id,
+            'Stranger' as promo_name,
+            0 as discount_euro,
+            0 as status_id,
+            'inactive' as status,
+            null as date_deleted,
+            null as _fivetran_synced_utc
     )
 
 select *
