bb9e77c0c041255a87844cc5a8a89b1c3f091fc9
Agustin Vissani <vissani1997@gmail.com>
2024-05-28 22:02:37 +0000

    users added

---
+++ models/staging/sqlserver/stg_sqlserver__users.sql
@@ -0,0 +1,22 @@
+with
+    source as (select * from {{ source("_sqlserver_sources", "users") }}),
+    src_sqlserver as (
+        select
+            user_id,
+            updated_at,
+            address_id,
+            last_name,
+            created_at,
+            phone_number,
+            total_orders,
+            first_name,
+            email,
+            _fivetran_deleted = 'true' as _fivetran_deleted,
+            _fivetran_synced,
+            convert_timezone('UTC', _fivetran_synced) as _fivetran_synced_utc
+        from source
+        where _fivetran_deleted is null
+    )
+
+select *
+from src_sqlserver
