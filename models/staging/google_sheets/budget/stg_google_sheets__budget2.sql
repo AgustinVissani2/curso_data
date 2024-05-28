221717b62873d4c6944d4f362d3954682a182de6
Agustin Vissani <vissani1997@gmail.com>
2024-05-28 12:11:21 +0000

    changes to the project structure and added events

--- models/staging/google_sheets/budget/stg_google_sheets__budget2.sql
+++ models/staging/google_sheets/budget/stg_google_sheets__budget2.sql
@@ -4,7 +4,13 @@ with
     src_budget as (select * from {{ ref("stg_google_sheets__budget") }}),
 
     renamed_casted as (
-        select _row, product_id, quantity, month
+        select
+            _row,
+            product_id,
+            quantity,
+            month as "DATE",
+            to_char(month, 'Month') as month_name,
+            date_load
         from src_budget
     )
 
