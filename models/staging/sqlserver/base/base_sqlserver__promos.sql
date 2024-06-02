42966963455e3d81cdec3aa3d1ca3aba9cc4a4b9
Agustin Vissani <vissani1997@gmail.com>
2024-06-02 15:48:47 +0000

    In this commit, the bases for promos and orders, shipping stgs, status, and promos were added. A macro was created to modify the name of the promo ids.

--- models/staging/sqlserver/base/base_sqlserver__promos.sql
+++ models/staging/sqlserver/base/base_sqlserver__promos.sql
@@ -2,21 +2,21 @@ with
     source as (select * from {{ source("_sqlserver_sources", "promos") }}),
     src_sqlserver as (
         select
-            md5(promo_id) as promo_id,
-            promo_id as promo_name,
+            case
+                when promo_id is null or promo_id = '' then 'unknown' else {{ normalize_promo_name('promo_id') }}
+            end as promo_name,
             discount as discount_euro,
-            iff(status = 'inactive', 0, 1) as status_id,
-            status as status_name,
+            status,
+            _fivetran_deleted,
             convert_timezone('UTC', _fivetran_synced) as _fivetran_synced_utc
         from source
         where _fivetran_deleted is null
         union all
         select
-            md5('Unknown') as promo_id,
-            'Unknown' as promo_name,
+            'unknown' as promo_name,
             0 as discount_euro,
-            0 as status_id,
-            'inactive' as status_name,
+            'unknown' as status,
+            null as _fivetran_deleted,
             null as _fivetran_synced_utc,
     )
 
--- models/staging/sqlserver/stg_sqlserver__promos.sql
+++ models/staging/sqlserver/stg_sqlserver__promos.sql
@@ -1,23 +1,16 @@
 with
-    source as (select * from {{ source("_sqlserver_sources", "promos") }}),
+    source as (select * from {{ ref("base_sqlserver__promos") }}),
+
     src_sqlserver as (
         select
-            md5(promo_id) as promo_id,
-            promo_id as promo_name,
-            discount as discount_euro,
-            iff(status = 'inactive', 0, 1) as status_id,
-            status as status_name,
-            convert_timezone('UTC', _fivetran_synced) as _fivetran_synced_utc
+            md5(promo_name) as promo_id,
+            promo_name,
+            discount_euro,
+            iff(status = 'inactive', 0, 1) as status,
+            _fivetran_deleted,
+            _fivetran_synced_utc
         from source
         where _fivetran_deleted is null
-        union all
-        select
-            md5('Unknown') as promo_id,
-            'Unknown' as promo_name,
-            0 as discount_euro,
-            0 as status_id,
-            'inactive' as status_name,
-            null as _fivetran_synced_utc,
     )
 
 select *
