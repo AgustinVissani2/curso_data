fb6f43476934bd108df1674d8b73505377ede0c5
Agustin Vissani <vissani1997@gmail.com>
2024-06-05 20:41:56 +0000

    change id promos

--- models/staging/sqlserver/stg_sqlserver__promos.sql
+++ models/staging/sqlserver/stg_sqlserver__promos.sql
@@ -7,32 +7,11 @@ with
             promo_name,
             discount_usd,
             status,
-            status_id,
+            iff(status = 'inactive', 0, 1) as status_id,
             _fivetran_deleted,
             _fivetran_synced_utc
         from source
         where _fivetran_deleted is null
-    ),
+    )
 
-    status_normalized as (
-        select distinct
-        status,
-        iff(status = 'inactive', 0, 1) as status_id
-        from src_sqlserver
-    ),
-
-promos_normalized as (
-    select
-            o.promo_id,
-            o.promo_name,
-            o.discount_usd,
-            sn.status_id,
-            sn.status,
-            o._fivetran_deleted,
-            o._fivetran_synced_utc
-    from src_sqlserver as o
-    left join status_normalized as sn
-        on o.status = sn.status
-)
-
-select * from promos_normalized
+select * from src_sqlserver
