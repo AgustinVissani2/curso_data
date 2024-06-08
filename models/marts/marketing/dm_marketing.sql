e12d5de131fe345e659217dbdbbb53c2c4052679
Agustin Vissani <vissani1997@gmail.com>
2024-06-08 19:19:48 +0000

    marketing 19.2

--- models/marts/marketing/dm_marketing.sql
+++ models/marts/marketing/dm_marketing.sql
@@ -1,60 +1,75 @@
 with
 
-fct_orders as (
-
-    select * from {{ ref('fct_orders') }}
-
+users AS (
+    SELECT * 
+    FROM {{ref('dim_users')}}
 ),
 
-dim_users as (
-
-    select * from {{ ref('dim_users') }}
-
+address AS (
+    SELECT *
+    FROM {{ref('dim_addresses')}}
 ),
 
-dim_promos as (
-
-    select * from {{ ref('dim_promos') }}
-
+products AS (
+    SELECT * 
+    FROM {{ref('dim_products')}}
 ),
 
-dim_addresses as (
+promos AS (
+    SELECT * 
+    FROM {{ref('dim_promos')}}
+),
 
-    select * from {{ ref('dim_addresses') }}
+orders AS (
+    SELECT * 
+    FROM {{ref('fct_orders')}}
+),
 
+promo_order as (
+    SELECT
+        o.order_id, 
+        sum(pr.discount_usd) as total_discount_usd
+    FROM orders o
+    left JOIN promos pr ON o.promo_id = pr.promo_id
+    group by all
+),
+
+orders_by_user AS (
+    SELECT 
+        u.user_id,
+        o.order_id,
+        o.order_total_usd,
+        o.shipping_cost_amount_usd,
+        count(distinct o.product_id) as total_different_product,
+        sum(o.quantity) as quantity
+    FROM users u
+    inner JOIN orders o ON u.user_id = o.user_id
+    group by all
 ),
 
 dm_marketing as (
-
-    select
-        o.user_id,
+    SELECT DISTINCT
+        u.user_id,
         u.first_name,
         u.last_name,
         u.email,
         u.phone_number,
-        u.created_at_time_utc,
-        u.updated_at_time_utc,
+        u.created_at_date,
+        u.updated_at_date,
         a.address,
         a.zipcode,
         a.state,
         a.country,
-        count(distinct order_id) as total_num_orders,
-        sum(pr.discount_usd) as total_discount_usd,
-        sum(order_total_usd) as total_orders_cost_usd,
-        sum(shipping_cost_amount_usd) as total_shipping_cost_usd,
-        sum(quantity) as total_quantity_products,
-        count(distinct product_id) as total_different_products,
-
-
-    from fct_orders as o
-    join dim_users as u
-        on o.user_id = u.user_id
-    join dim_addresses as a
-        on u.address_id = a.address_id
-    join dim_promos as pr
-        on o.promo_id = pr.promo_id
-
-    
+        count(distinct ou.order_id) as total_number_orders,
+        sum(ou.order_total_usd) as total_order_cost_usd,
+        sum(ou.shipping_cost_amount_usd) as total_shipping_cost_usd,
+        pr.total_discount_usd,
+        sum(ou.total_different_product) as total_different_product,
+        sum(ou.quantity) as total_quantity_product
+    FROM users u
+    RIGHT JOIN orders_by_user ou ON u.user_id = ou.user_id
+    inner JOIN address a ON u.address_id = a.address_id
+    inner join promo_order pr ON ou.order_id = pr.order_id
     group by all
 )
 
