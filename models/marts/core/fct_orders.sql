2875bf122fcd9b7a86e5e48797300a570cad08c3
Agustin Vissani <vissani1997@gmail.com>
2024-06-10 11:38:40 +0000

    add time and date in orders

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -20,10 +20,12 @@ join_order_Ordersitems as (
         o.order_total_usd,
         i.product_id,
         i.quantity,
-        o.created_at_utc,
-        o.created_at_month,
-        o.estimated_delivery_at_utc,
-        o.delivered_at_utc
+        o.created_at_date,
+        o.created_at_time_utc,
+        o.estimated_delivery_at_date,
+        o.estimated_delivery_at_time_utc,
+        o.delivered_at_date,
+        o.delivered_at_time_utc,
     from source_orders as o
     inner join source_order_items as i
         on o.order_id = i.order_id
--- tests/later_delivery_date.sql
+++ tests/later_delivery_date.sql
@@ -1,4 +1,4 @@
 SELECT *
 FROM {{ ref('stg_sqlserver__orders') }}
-WHERE delivered_at_utc < created_at_utc
+WHERE delivered_at_date < created_at_date AND delivered_at_time_utc < created_at_time_utc
 -- checks if there is any case where the delivery date is earlier than the date the order was placed, which would not make logical sense and would indicate some kind of problem with the data 
