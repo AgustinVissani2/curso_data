64d55c0c2aa47de3d5a374f42e8f6316b2d94c0f
Agustin Vissani <vissani1997@gmail.com>
2024-06-12 18:01:37 +0000

    budget

--- models/staging/google_sheets/stg_google_sheets__budget.sql
+++ models/staging/google_sheets/stg_google_sheets__budget.sql
@@ -4,7 +4,7 @@ src_budget as (select * from {{ source("_google_sheets__sources", "budget") }}),
 renamed_casted as (
     select
         {{ dbt_utils.generate_surrogate_key(['_row']) }} AS budget_id,
-        product_id,
+        {{ dbt_utils.generate_surrogate_key(['product_id']) }} AS product_id,
         quantity as quantity_sold_expected,
         month as "DATE",
         convert_timezone('UTC', _fivetran_synced) as _fivetran_synced_utc
