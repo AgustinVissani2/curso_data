4eb80e8a031e637bd726372fe29d093f4aed4342
Agustin Vissani <vissani1997@gmail.com>
2024-06-13 20:36:26 +0000

    add yml core

--- macros/_sql_server_dbo__sources.sql
+++ macros/_sql_server_dbo__sources.sql
@@ -11,3 +11,33 @@
         include_schema=True
         )
 }}
+
+{{
+    codegen.generate_source(
+        schema_name = 'CORE',
+        database_name = 'ALUMNO4_DEV_GOLD_DB',
+        table_names = ['dim_status_orders', 'dim_products', 'dim_promos', 'dim_users', 'fct_orders', 'fct_events' ],
+        generate_columns = True,
+        include_descriptions=True,
+        include_data_types=True,
+        name='_core',
+        include_database=True,
+        include_schema=True
+        )
+}}
+
+
+
+{{
+    codegen.generate_source(
+        schema_name = 'CORE',
+        database_name = 'ALUMNO4_DEV_GOLD_DB',
+        table_names = ['dim_addresses', 'dim_event_types','dim_month', 'dim_time', 'dim_shipping', 'dim_status_promos' ],
+        generate_columns = True,
+        include_descriptions=True,
+        include_data_types=True,
+        name='_core',
+        include_database=True,
+        include_schema=True
+        )
+}}
