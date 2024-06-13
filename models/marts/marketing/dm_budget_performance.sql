a5f1ef38b8eccd28f146a076433bf73e346ca99c
Agustin Vissani <vissani1997@gmail.com>
2024-06-13 17:10:42 +0000

    dm budget no ready

--- models/marts/marketing/dm_budget_performance.sql
+++ models/marts/marketing/dm_budget_performance.sql
@@ -1,11 +1,16 @@
 WITH budgets AS (
     SELECT * 
-    FROM {{ref('fct_budget')}}
+    FROM {{ref('stg_google_sheets__budget')}}
 ),
 
 products AS (
     SELECT * 
     FROM {{ref('dim_products')}}
+),
+
+orders AS (
+    SELECT * 
+    FROM {{ref('fct_orders')}}
 ),
 
 budget_details AS (
@@ -15,23 +20,15 @@ budget_details AS (
         p.product_name,
         p.price_usd,
         b.quantity_sold_expected,
+        SUM(o.quantity) AS quantity_sold_actual,
+        (b.quantity_sold_expected - SUM(o.quantity)) AS quantity_remaining,
+        SUM(o.order_total_usd) AS total_sales_usd,
+        SUM(o.order_total_usd) - (b.quantity_sold_expected * p.price_usd) AS variance_usd
     FROM budgets b
     LEFT JOIN products p ON b.product_id = p.product_id
-
+    LEFT JOIN orders o ON b.product_id = o.product_id
     GROUP BY all
-),
-
-budget_user_details AS (
-    SELECT
-        bd.budget_id,
-        bd.product_id,
-        bd.product_name,
-        bd.price_usd,
-        bd.quantity_sold_expected,
-        (price_usd * quantity_sold_expected) as  expected_sales_usd
-    FROM budget_details bd
 )
 
-SELECT DISTINCT
-   *
-FROM budget_user_details
+SELECT *
+FROM budget_details
